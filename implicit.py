b6f8174b8a166b74c3c6ad121dafcebea60cbf80
harrysdc <39647707+harrysdc@users.noreply.github.com>
2023-12-03 19:19:54 -0500

    first commit

---
+++ implicit.py
@@ -0,0 +1,316 @@
+import torch
+import torch.nn.functional as F
+
+from ray_utils import RayBundle
+from typing import Tuple
+
+
+# Sphere SDF class
+class SphereSDF(torch.nn.Module):
+    def __init__(
+        self,
+        cfg
+    ):
+        super().__init__()
+
+        self.radius = torch.nn.Parameter(
+            torch.tensor(cfg.radius.val).float(), requires_grad=cfg.radius.opt
+        )
+        self.center = torch.nn.Parameter(
+            torch.tensor(cfg.center.val).float().unsqueeze(0), requires_grad=cfg.center.opt
+        )
+
+    def forward(self, ray_bundle):
+        sample_points = ray_bundle.sample_points.reshape(-1, 3)
+
+        return torch.linalg.norm(
+            sample_points - self.center,
+            dim=-1,
+            keepdim=True
+        ) - self.radius
+
+
+# Box SDF class
+class BoxSDF(torch.nn.Module):
+    def __init__(
+        self,
+        cfg
+    ):
+        super().__init__()
+
+        self.center = torch.nn.Parameter(
+            torch.tensor(cfg.center.val).float().unsqueeze(0), requires_grad=cfg.center.opt
+        )
+        self.side_lengths = torch.nn.Parameter(
+            torch.tensor(cfg.side_lengths.val).float().unsqueeze(0), requires_grad=cfg.side_lengths.opt
+        )
+
+    def forward(self, ray_bundle):
+        sample_points = ray_bundle.sample_points.reshape(-1, 3)
+        diff = torch.abs(sample_points - self.center) - self.side_lengths / 2.0
+
+        signed_distance = torch.linalg.norm(
+            torch.maximum(diff, torch.zeros_like(diff)),
+            dim=-1
+        ) + torch.minimum(torch.max(diff, dim=-1)[0], torch.zeros_like(diff[..., 0]))
+
+        return signed_distance.unsqueeze(-1)
+
+
+sdf_dict = {
+    'sphere': SphereSDF,
+    'box': BoxSDF,
+}
+
+
+# Converts SDF into density/feature volume
+class SDFVolume(torch.nn.Module):
+    def __init__(
+        self,
+        cfg
+    ):
+        super().__init__()
+
+        self.sdf = sdf_dict[cfg.sdf.type](
+            cfg.sdf
+        )
+
+        self.rainbow = cfg.feature.rainbow if 'rainbow' in cfg.feature else False
+        self.feature = torch.nn.Parameter(
+            torch.ones_like(torch.tensor(cfg.feature.val).float().unsqueeze(0)), requires_grad=cfg.feature.opt
+        )
+
+        self.alpha = torch.nn.Parameter(
+            torch.tensor(cfg.alpha.val).float(), requires_grad=cfg.alpha.opt
+        )
+        self.beta = torch.nn.Parameter(
+            torch.tensor(cfg.beta.val).float(), requires_grad=cfg.beta.opt
+        )
+
+    def _sdf_to_density(self, signed_distance):
+        # Convert signed distance to density with alpha, beta parameters
+        return torch.where(
+            signed_distance > 0,
+            0.5 * torch.exp(-signed_distance / self.beta),
+            1 - 0.5 * torch.exp(signed_distance / self.beta),
+        ) * self.alpha
+
+    def forward(self, ray_bundle):
+        sample_points = ray_bundle.sample_points.reshape(-1, 3)
+        depth_values = ray_bundle.sample_lengths[..., 0]
+        deltas = torch.cat(
+            (
+                depth_values[..., 1:] - depth_values[..., :-1],
+                1e10 * torch.ones_like(depth_values[..., :1]),
+            ),
+            dim=-1,
+        ).view(-1, 1)
+
+        # Transform SDF to density
+        signed_distance = self.sdf(ray_bundle)
+        density = self._sdf_to_density(signed_distance)
+
+        # Outputs
+        if self.rainbow:
+            base_color = torch.clamp(
+                torch.abs(sample_points - self.sdf.center),
+                0.02,
+                0.98
+            )
+        else:
+            base_color = 1.0
+
+        out = {
+            'density': -torch.log(1.0 - density) / deltas,
+            'feature': base_color * self.feature * density.new_ones(sample_points.shape[0], 1)
+        }
+
+        return out
+
+
+class HarmonicEmbedding(torch.nn.Module):
+    def __init__(
+        self,
+        n_harmonic_functions: int = 6,
+        omega0: float = 1.0,
+        logspace: bool = True,
+        include_input: bool = True,
+    ) -> None:
+        super().__init__()
+
+        if logspace:
+            frequencies = 2.0 ** torch.arange(
+                n_harmonic_functions,
+                dtype=torch.float32,
+            )
+        else:
+            frequencies = torch.linspace(
+                1.0,
+                2.0 ** (n_harmonic_functions - 1),
+                n_harmonic_functions,
+                dtype=torch.float32,
+            )
+
+        self.register_buffer("_frequencies", frequencies * omega0, persistent=False)
+        self.append_input = include_input
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        embed = (x[..., None] * self._frequencies).reshape(*x.shape[:-1], -1)
+        embed = torch.cat(
+            (embed.sin(), embed.cos(), x)
+            if self.append_input
+            else (embed.sin(), embed.cos()),
+            dim=-1,
+        )
+        return embed
+
+
+class LinearWithRepeat(torch.nn.Linear):
+    def forward(self, input):
+        n1 = input[0].shape[-1]
+        output1 = F.linear(input[0], self.weight[:, :n1], self.bias)
+        output2 = F.linear(input[1], self.weight[:, n1:], None)
+        return output1 + output2.unsqueeze(-2)
+
+
+class MLPWithInputSkips(torch.nn.Module):
+    """
+    A sequence of linear layers with ReLU activations
+    Additionally, for a set of predefined layers `input_skips`, the forward pass sappends a skip tensor `z`
+    """
+    def __init__(
+        self,
+        n_layers: int,
+        input_dim: int,
+        output_dim: int,
+        skip_dim: int,
+        hidden_dim: int,
+        input_skips: Tuple[int, ...] = (),
+    ):
+        super().__init__()
+        layers = []
+        for layeri in range(n_layers):
+            if layeri == 0:
+                dimin = input_dim
+                dimout = hidden_dim
+            elif layeri in input_skips:
+                dimin = hidden_dim + skip_dim
+                dimout = hidden_dim
+            else:
+                dimin = hidden_dim
+                dimout = hidden_dim
+
+            linear = torch.nn.Linear(dimin, dimout)
+            _xavier_init(linear)
+            layers.append(torch.nn.Sequential(linear, torch.nn.ReLU(True)))
+
+        self.mlp = torch.nn.ModuleList(layers)
+        self._input_skips = set(input_skips)
+
+    def forward(self, x: torch.Tensor, z: torch.Tensor) -> torch.Tensor:
+        y = x
+        for li, layer in enumerate(self.mlp):
+            if li in self._input_skips:
+                y = torch.cat((y, z), dim=-1)
+
+            y = layer(y)
+        return y
+
+def _xavier_init(linear):
+    """
+    Performs the Xavier weight initialization of the linear layer `linear`.
+    """
+    torch.nn.init.xavier_uniform_(linear.weight.data)
+
+# TODO (3.1): Implement NeRF MLP
+class NeuralRadianceField(torch.nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+
+        # harmonic embedding of 3D point locations
+        self.harmonic_embedding_xyz = HarmonicEmbedding(cfg.n_harmonic_functions_xyz)
+        # harmonic embedding of the ray directions
+        self.harmonic_embedding_dir = HarmonicEmbedding(cfg.n_harmonic_functions_dir)
+        embedding_dim_xyz = cfg.n_harmonic_functions_xyz * 2 * 3 + 3
+        embedding_dim_dir = cfg.n_harmonic_functions_dir * 2 * 3 + 3
+
+        self.mlp_xyz = MLPWithInputSkips(
+            n_layers = cfg.n_layers_xyz,
+            input_dim = embedding_dim_xyz,
+            output_dim = cfg.n_hidden_neurons_xyz,
+            skip_dim = embedding_dim_xyz,
+            hidden_dim = cfg.n_hidden_neurons_xyz,
+            input_skips = cfg.append_xyz,
+        )
+
+        self.intermediate_linear = torch.nn.Linear(cfg.n_hidden_neurons_xyz, cfg.n_hidden_neurons_xyz)
+        _xavier_init(self.intermediate_linear)
+
+        self.density_layer = torch.nn.Linear(cfg.n_hidden_neurons_xyz, 1)
+        _xavier_init(self.density_layer)
+        self.density_layer.bias.data[:] = 0.0
+
+        self.color_layer = torch.nn.Sequential(
+            LinearWithRepeat(cfg.n_hidden_neurons_xyz + embedding_dim_dir, cfg.n_hidden_neurons_dir),
+            torch.nn.ReLU(True),
+            torch.nn.Linear(cfg.n_hidden_neurons_dir, 3),
+            torch.nn.Sigmoid(),
+        )
+
+
+    def get_densities(self, features):
+        """
+        Convert feature to raw_densities with self.density_layer
+        """
+        raw_densities = self.density_layer(features)
+        densities = torch.relu(raw_densities)
+        return densities
+
+
+    def get_colors(self, features: torch.Tensor, rays_directions: torch.Tensor):
+        """
+        Takes per-point features and evaluates the color 
+        in order to attach to each point its RGB color
+        """
+        rays_directions_normed = torch.nn.functional.normalize(rays_directions, dim=-1)
+        rays_embedding = self.harmonic_embedding_dir(rays_directions_normed)
+
+        return self.color_layer((self.intermediate_linear(features), rays_embedding))
+
+    def get_densities_and_colors(self, features, ray_bundle: RayBundle) -> Tuple[torch.Tensor, torch.Tensor]:
+        """
+        The 2nd part of the forward pass
+        features -- the output of mlp (minibatch x ... x self.n_hidden_neurons_xyz)
+        """
+        rays_densities = self.get_densities(features)
+        rays_colors = self.get_colors(features, ray_bundle.directions)
+        return rays_densities, rays_colors
+
+    def forward(self, ray_bundle: RayBundle, **kwargs) -> Tuple[torch.Tensor, torch.Tensor]:
+        """
+        1st part: Use MLP to map embeddings to a latent feature space
+        2nd part: Get density and colors
+
+        Returns:
+        rays_densities: opacity of each ray point (minibatch, ..., num_points_per_ray, 1)
+        rays_colors: color of each ray point (minibatch, ..., num_points_per_ray, 3)
+        """
+        # [minibatch x ... x self.n_harmonic_functions*6 + 3]
+        embeds_xyz = self.harmonic_embedding_xyz(ray_bundle.sample_points)
+        
+
+        # [minibatch x ... x self.n_hidden_neurons_xyz]
+        features = self.mlp_xyz(embeds_xyz, embeds_xyz)
+        rays_densities, rays_colors = self.get_densities_and_colors(features, ray_bundle)
+        
+        out = {
+            'density': rays_densities,
+            'feature': rays_colors,
+        }
+        return out
+
+
+volume_dict = {
+    'sdf_volume': SDFVolume,
+    'nerf': NeuralRadianceField,
+}
---
+++ renderer.py
@@ -0,0 +1,110 @@
+import torch
+
+from typing import List, Optional, Tuple
+from pytorch3d.renderer.cameras import CamerasBase
+
+
+class VolumeRenderer(torch.nn.Module):
+    """
+    Integrates color and density along rays according to 
+    the equations defined in [Mildenhall et al. 2020]
+    """
+    def __init__(self,cfg):
+        super().__init__()
+
+        self._chunk_size = cfg.chunk_size
+        self._white_background = cfg.white_background if 'white_background' in cfg else False
+
+    def _compute_weights(self,deltas,rays_density: torch.Tensor,eps: float = 1e-10):
+        """
+        deltas      ([1, num_rays * num_sample_per_ray, 1]) = ([1, 4194304, 1])
+        ray density ([1, num_rays * num_sample_per_ray, 1]) = ([1, 4194304, 1])
+        """
+
+        # TODO (1.5): Compute transmittance using the equation described in the README
+        deltas = deltas.squeeze()
+        rays_density = rays_density.squeeze()
+        alpha = 1 - torch.exp(-rays_density * deltas)
+        
+        # TODO (1.5): Compute weight used for rendering from transmittance and density
+        weights = self._compute_accumulated_transmittance(1 - alpha).unsqueeze(2) * alpha.unsqueeze(2)
+        return weights
+
+    def _compute_accumulated_transmittance(self, alphas):
+        accumulated_transmittance = torch.cumprod(alphas, 1)
+        return torch.cat((torch.ones((accumulated_transmittance.shape[0], 1), device=alphas.device),
+                      accumulated_transmittance[:, :-1]), dim=-1)
+
+    def _aggregate(self,weights: torch.Tensor,rays_feature: torch.Tensor):
+        """
+        Aggregate sample points along a ray
+        weights      ([1, 4194304, 1])
+        rays_feature ([4194304, 3])
+        """        
+        
+        # TODO (1.5): Aggregate (weighted sum of) features using weights
+        feature = torch.sum(rays_feature * weights, dim=1)
+        return feature
+
+    def forward(self, sampler, implicit_fn, ray_bundle):
+        B = ray_bundle.shape[0]
+
+        # Process the chunks of rays.
+        chunk_outputs = []
+
+        for chunk_start in range(0, B, self._chunk_size):
+            cur_ray_bundle = ray_bundle[chunk_start:chunk_start+self._chunk_size]
+
+            # Sample points along the ray
+            cur_ray_bundle = sampler(cur_ray_bundle)
+            n_pts = cur_ray_bundle.sample_shape[1]
+
+            # Call implicit function with sample points
+            implicit_output = implicit_fn(cur_ray_bundle)
+            density = implicit_output['density']
+            feature = implicit_output['feature']
+
+            # Compute length of each ray segment
+            depth_values = cur_ray_bundle.sample_lengths[..., 0]
+            deltas = torch.cat(
+                (
+                    depth_values[..., 1:] - depth_values[..., :-1],
+                    1e10 * torch.ones_like(depth_values[..., :1]),
+                ),
+                dim=-1,
+            )[..., None]
+
+            # Compute aggregation weights
+            weights = self._compute_weights(
+                deltas.view(-1, n_pts, 1),
+                density.view(-1, n_pts, 1)
+            )
+
+            # TODO (1.5): Render (color) features using weights
+            feature = self._aggregate(weights, feature.view(-1, weights.shape[1], 3))
+
+            # TODO (1.5): Render depth map
+            depth = self._aggregate(weights, depth_values.view(-1, weights.shape[1], 1))
+
+            # Return
+            cur_out = {
+                'feature': feature,
+                'depth': depth,
+            }
+
+            chunk_outputs.append(cur_out)
+
+        # Concatenate chunk outputs
+        out = {
+            k: torch.cat(
+              [chunk_out[k] for chunk_out in chunk_outputs],
+              dim=0
+            ) for k in chunk_outputs[0].keys()
+        }
+
+        return out
+
+
+renderer_dict = {
+    'volume': VolumeRenderer
+}
